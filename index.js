d1903cde0e6c034c2cfaa73eeabc0ffbc4266a85
Bea Obiero <bea.obiero@gmail.comgit config --global user.email bea.obiero@gmail.comgit config --global user.email + + bea.obiero@gmail.com>
2023-12-20 23:53:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+//declaring the global scope
+var customerName = 'BOB';
+
+console.log(window.customerName);
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+    function setBestCustomer() {
+    bestCustomer = 'not bob';
+    }
+
+    
+function overwriteBestCustomer(){
+    bestCustomer = 'new customer';
+}
+console.log(window.bestCustomer);
+
+const leastFavoriteCustomer = 'unknown';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'new person'
+}
